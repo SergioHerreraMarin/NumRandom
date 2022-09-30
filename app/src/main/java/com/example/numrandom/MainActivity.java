ae6d46a1dd9013622f75fb00b1f09475a1934f5a
SergioHerreraMarin <sergioherreramarin@gmail.com>
2022-09-30 19:16:51 +0200

    Lo mismo que el anterior

--- app/src/main/java/com/example/numrandom/MainActivity.java
+++ app/src/main/java/com/example/numrandom/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.numrandom;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,7 +22,13 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    int numIntentos = 0;
+    private int numIntentos = 0;
+    private int minNumIntentos = 100;
+    private int numRandom;
+    private TextView textIntentos;
+    private TextView textResult;
+    Random random = new Random();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,46 +36,77 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Context context = getApplicationContext();
 
-        Random random = new Random();
+        numRandom = random.nextInt(11);
+
         Button button = findViewById(R.id.button2);
-
-        final int numRandom = random.nextInt(11);
+        Button buttonRanking = findViewById(R.id.buttonRanking);
         EditText inputText = findViewById(R.id.inputText);
-        TextView textResult = findViewById(R.id.textresult);
-        TextView textIntentos = findViewById(R.id.textIntentos);
-
-
+        textResult = findViewById(R.id.textresult);
+        textIntentos = findViewById(R.id.textIntentos);
+        textResult.setMovementMethod(new ScrollingMovementMethod());
 
         button.setOnClickListener(new View.OnClickListener(){
+
             @Override
             public void onClick(View view) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Has adivinado el número").setTitle("CORRECTO").setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        resetGame();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
 
                 String message = "";
                 Editable editable = inputText.getText();
                 int userNum = Integer.parseInt(editable.toString());
+                inputText.setText("");
 
                 if(userNum < numRandom){
-                    message = "El número es más grande";
+                    message = "El número " + userNum + " es más pequeño";
                 }else if(userNum > numRandom){
-                    message = "El número es más pequeño";
+                    message = "El número " + userNum + " es más grande";
                 }else{
                     message = "Acertaste el número!";
+                    dialog.show();
                 }
 
                 numIntentos++;
                 textIntentos.setText("Intentos: " + numIntentos);
-                textResult.setText(message);
+                textResult.append(message + "\n");
 
-                /*
+
                 Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
                 toast.show();
-                */
+
+
             }
         });
 
 
+        buttonRanking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, Ranking.class);
+                intent.putExtra("minIntentos",minNumIntentos ); //pasar valores
+                startActivity(intent);
+            }
+        });
+
+    }
 
 
-
+    private void resetGame(){
+        numRandom = random.nextInt(11);
+        if(numIntentos < minNumIntentos){
+            minNumIntentos = numIntentos;
+        }
+        numIntentos = 0;
+        textIntentos.setText("Intentos: " + numIntentos);
+        textResult.setText("");
     }
 }
--- app/src/main/java/com/example/numrandom/Ranking.java
+++ app/src/main/java/com/example/numrandom/Ranking.java
@@ -1,14 +1,43 @@
 package com.example.numrandom;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Ranking extends AppCompatActivity {
+
+    private TextView minIntentosText;
+    private int minNumIntentos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
+
+        minIntentosText = findViewById(R.id.textViewMinIntentos);
+
+        Intent intent = getIntent();
+        minNumIntentos = intent.getIntExtra("minIntentos", 0);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(Ranking.this);
+        builder.setMessage("Quieres introducir la puntuación?").setTitle("Ranking").setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                minIntentosText.setText("MinIntentos: " + minNumIntentos);
+            }
+        }).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                minIntentosText.setText("MinIntentos: " + 0);
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
     }
 }
